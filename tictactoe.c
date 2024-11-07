436a4a48169eccd5c5ff17a56c28a46411ce44c8
Alex <alexander.mcisaac@student.tdsb.on.ca>
2024-11-07 14:02:37 -0500

    init commit

---
+++ tictactoe.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 10
+#define MIN_SIZE 3
+
+int main() {
+
+    return 0;
+}
+
+initializeBoard(){}
+
+displayBoard(){}
+
+playerMove(){}
+
+aiMove(){}
+
+checkDraw(){}
+
+updateScore(){}
+
